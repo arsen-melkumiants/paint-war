26744d5d852554c15cc356afd1108a331b0e636e
Arsen Melkumiants <arsen.mem@gmail.com>
2014-02-05 16:25:41 -0500

    delete temp file

--- application/config/add_constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('SITE_NAME', 'PaintWar');
-define('SITE_DESCR', 'Организация paintball-матчей');
-define('EQUIP_PRICE', '30');
-define('BALL_PRICE', '15');
-define('RAW_BALL_PRICE', '10');
-define('BALLS_IN_BOX', '2000');
